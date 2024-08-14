98b27a47049828de667e21a92a10c33eee26b213
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-08-14 15:05:53 +0700

    .

---
+++ BaiLam/src/main/java/com/example/bailam/BaiLamApplication.java
@@ -0,0 +1,13 @@
+package com.example.bailam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiLamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiLamApplication.class, args);
+    }
+
+}
